62da4bec7fc61e02d3480a7bbeaf58eefc542a90
asm-dev <salamanca.m@outlook.com>
2024-11-28 16:16:52 +0100

    follow spotify app authorisation guidelines for login req

--- service/spotify_service.py
+++ service/spotify_service.py
@@ -1,9 +1,27 @@
 from dotenv import load_dotenv
-import requests
+from fastapi.responses import RedirectResponse
 import os
 
 #Loads .env file
 load_dotenv()
 
 API_BASE_URL = "https://api.spotify.com/v1"
-TOKEN = os.getenv("SPOTY_TOKEN")
+
+SPOTY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTY_REDIRECT_URL = os.getenv("SPOTIFY_REDIRECT")
+SPOTY_AUTH_URL = "https://accounts.spotify.com/authorize"
+
+@app.get("/login")
+def login():
+    #TODO: Conseguir parametros para auth, falta scope y state SEGURIDAD!
+    query_data = {
+        "response_type": "code",
+        "client_id":SPOTY_CLIENT_ID,
+        "redirect_url": SPOTY_REDIRECT_URL
+    }
+
+    auth_redirect_url = f"{SPOTY_AUTH_URL}?{(query_data)}"
+
+    http_redirect_response = RedirectResponse(auth_redirect_url)
+
+    return http_redirect_response
